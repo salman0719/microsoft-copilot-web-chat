017389185f9b51db6a3c2381c5658d8a9c0eda8e
Salman <salmanhn719@gmail.com>
2024-08-15 22:31:29 +1000

    Scout mistake disclosure text insertion

--- public/js/webchat.js
+++ public/js/webchat.js
@@ -16,12 +16,25 @@ async function fetchJSON(url, options = {}) {
 }
 
 (async function main() {
+  let initialized = false
+
   // This is for obtaining Direct Line token from the bot.
   const { token } = await fetchJSON('/api/directline/token');
 
   // Triggers bot with initial message, in order to have greeting message render on load.
   const store = WebChat.createStore({}, ({ dispatch }) => next => action => {
     const { type } = action;
+
+    if (!initialized) {
+      initialized = true
+
+      const sendBoxElem = document.querySelector('#webchat .webchat__send-box')
+      const disclosureText = document.createElement('div')
+      disclosureText.innerHTML = 'Scout can make mistakes, verify important information.'
+      disclosureText.className = 'webchat__send-box-info'
+      sendBoxElem.appendChild(disclosureText)
+    }
+
 
     if (type === 'DIRECT_LINE/CONNECT_FULFILLED') {
       const text = 'I need help with submitting my assessment.'
@@ -51,4 +64,5 @@ async function fetchJSON(url, options = {}) {
     },
     document.getElementById('webchat')
   );
+
 })().catch(err => console.error(err));
