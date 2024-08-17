f1c6dbb97a373099a26d8336e0cd2b51f22270f3
Salman <salmanhn719@gmail.com>
2024-08-18 03:40:40 +1000

    Bundling through esbuild

--- public/js/webchat.js
+++ public/js/webchat.js
@@ -19,54 +19,56 @@ import {
   updateTimestamp
 } from "./utils/helper.js";
 
-// TODO
-// This is for obtaining Direct Line token from the bot.
-// Should be replaced with your own mechanism
-const { token } = await fetchToken();
+(async function main() {
+  // TODO
+  // This is for obtaining Direct Line token from the bot.
+  // Should be replaced with your own mechanism
+  const { token } = await fetchToken();
 
-const store = WebChat.createStore({}, () => next => action => {
-  const { type, payload } = action
+  const store = WebChat.createStore({}, () => next => action => {
+    const { type, payload } = action
 
-  if (type === 'DIRECT_LINE/CONNECT_FULFILLED') {
-    updateConversationId(payload.directLine.conversationId)
-    setData('username', action.meta.username)
-  } else if (type === 'DIRECT_LINE/CONNECTION_STATUS_UPDATE') {
-    if (payload.connectionStatus === 2) {
-      setTimeout(() => {
-        setData('isDarkMode', localStorage.getItem(WEBCHAT_MODE_KEY) === '1')
-        initiateChatPrompt()
-        setData('isClosed', localStorage.getItem(WEBCHAT_WINDOW_CLOSED_KEY) === '1')
-      }, 500)
+    if (type === 'DIRECT_LINE/CONNECT_FULFILLED') {
+      updateConversationId(payload.directLine.conversationId)
+      setData('username', action.meta.username)
+    } else if (type === 'DIRECT_LINE/CONNECTION_STATUS_UPDATE') {
+      if (payload.connectionStatus === 2) {
+        setTimeout(() => {
+          setData('isDarkMode', localStorage.getItem(WEBCHAT_MODE_KEY) === '1')
+          initiateChatPrompt()
+          setData('isClosed', localStorage.getItem(WEBCHAT_WINDOW_CLOSED_KEY) === '1')
+        }, 500)
+      }
+    } else if (type === 'DIRECT_LINE/INCOMING_ACTIVITY') {
+      updateTimestamp(payload.activity)
     }
-  } else if (type === 'DIRECT_LINE/INCOMING_ACTIVITY') {
-    updateTimestamp(payload.activity)
-  }
 
-  return next(action);
-});
+    return next(action);
+  });
 
-WebChat.renderWebChat(
-  {
-    directLine: WebChat.createDirectLine({ token }),
-    store,
-    styleOptions: {
-      primaryFont: ['Roboto', 'Calibri', 'Helvetica Neue', 'Arial', 'sans-serif']
-        .map(font => `'${font}'`).join(', '),
-      botAvatarImage: '../images/bot.png',
-      botAvatarInitials: 'S',
+  WebChat.renderWebChat(
+    {
+      directLine: WebChat.createDirectLine({ token }),
+      store,
+      styleOptions: {
+        primaryFont: ['Roboto', 'Calibri', 'Helvetica Neue', 'Arial', 'sans-serif']
+          .map(font => `'${font}'`).join(', '),
+        botAvatarImage: '../images/bot.png',
+        botAvatarInitials: 'S',
+      },
     },
-  },
-  document.getElementById('webchat')
-);
+    document.getElementById('webchat')
+  );
 
-setData('webChatStore', store)
-setData('chatPromptInitialized', false)
-handleUsername()
-handleInput()
-insertDisclosureText()
-updateInputPlaceholder()
-setupExpandIcon()
-setupWindowToggle()
-setupCondensation()
-insertInputCounter()
-setupModeToggle()
+  setData('webChatStore', store)
+  setData('chatPromptInitialized', false)
+  handleUsername()
+  handleInput()
+  insertDisclosureText()
+  updateInputPlaceholder()
+  setupExpandIcon()
+  setupWindowToggle()
+  setupCondensation()
+  insertInputCounter()
+  setupModeToggle()
+})().catch(err => console.error(err))
