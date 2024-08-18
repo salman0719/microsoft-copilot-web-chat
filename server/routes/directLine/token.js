9a9743abf23bb876670067ccc89801e2000c5dff
Salman <salmanhn719@gmail.com>
2024-08-18 22:05:04 +1000

    Caching token result

--- server/routes/directLine/token.js
+++ server/routes/directLine/token.js
@@ -3,5 +3,6 @@ const generateDirectLineToken = require('../../generateDirectLineToken');
 // This module exports token value to /api/directline/token and can be accessed with GET
 // Generates a new Direct Line token
 module.exports = async (_, res) => {
+  res.cache('public', { maxAge: 3000 })
   res.json(await generateDirectLineToken());
 };
